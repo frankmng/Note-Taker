7cf780753f8a33e5b00a5c669fffb12a0990cfa1
Frank Nguyen <frank@ultra.me>
2022-12-16 19:33:49 -0800

    added function to add and save notes

--- Develop/server.js
+++ Develop/server.js
@@ -2,26 +2,62 @@ const express = require('express');
 const fs = require("fs")
 const path = require('path');
 const app = express();
-
+const bodyParser = require('body-parser');
 // Require the JSON file and assign it to a variable called `termData`
 const db = require('./db/db.json')
 
 const PORT = 3001;
 
 app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'))
-})
+// Middleware to parse incoming Request Object as a JSON Object 
+app.use(bodyParser.json());
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/')));
 
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/notes.html'))
-})
+});
 
+// GET request to retrieve notes
 app.get('/api/notes', (req, res) => {
-  res.json(db)
-})
+  res.status(200).json(db)
+});
+
+// POST request to add a note
+app.post('/api/notes', (req, res) => {
+
+  const { title, text } = req.body;
+
+  if (title && text){
+      const newNote =  {
+        title,
+        text,
+      };
+
+      fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          const parsedNotes = JSON.parse(data);
+
+          parsedNotes.push(newNote);
+          fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 4),
+          (writeErr) =>
+          writeErr
+            ? console.error(writeErr)
+            : console.info('Successfully updated notes!')
+          );
+        }
+      })
+      const response = {
+        status: 'success',
+        body: newNote,
+      }
+      res.status(201).json(response);
+      } else {
+        res.status(500).json('Error in adding note')
+      }
+});
 
 app.listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}`)
-})
+  console.log(`Server listening on PORT ${PORT}`)
+});
